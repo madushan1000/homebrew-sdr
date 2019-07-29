56d04e79f89586ea1607ba22de23a4aeb3ace1d9
Madushan Nishantha <j.l.madushan@gmail.com>
2019-07-29 23:50:02 +0530

    fix script pythonpath

--- gnuradio.rb
+++ gnuradio.rb
@@ -115,7 +115,7 @@ class Gnuradio < Formula
     end
 
     rm bin.children.reject(&:executable?)
-    bin.env_script_all_files(libexec/"bin", :PYTHONPATH => ENV["PYTHONPATH"])
+    bin.env_script_all_files(libexec/"bin", :PYTHONPATH => "#{ENV['PYTHONPATH']}:$PYTHONPATH") 
   end
 
   test do
